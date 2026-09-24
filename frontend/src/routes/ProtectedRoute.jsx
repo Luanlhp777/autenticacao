80db3f9b19b90bb2e179c537c8994570add183f8
Aluno <e023aluno@outlook.com>
2026-09-23 22:07:00 -0300

    fix: corrige estrutura e dependencias do frontend

---
+++ frontend/src/routes/ProtectedRoute.jsx
@@ -0,0 +1,14 @@
+import { Navigate } from 'react-router-dom';
+
+export default function ProtectedRoute({ isAuth, carregando, children }){
+    
+    if(carregando){
+        return <div className='loading'>Verificando autenticação...</div>
+    }
+
+    if(isAuth){
+        return <Navigate to="login" replace />
+    }
+
+    return children;
+}
